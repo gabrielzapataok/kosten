e4d304c0c25bbdccec2d3fa92779dda3b7457983
gabrielzapata <gaby.fz.58@gmail.com>
2024-03-01 19:22:23 -0300

    kosten click home

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const swiperHedaer = new Swiper('.header-slider-videos .swiper', {
             var h1Element = currentSlide.querySelector('h1')
             setTimeout(function () {
               $(h1Element).fadeIn()
-            }, 1000)
+            }, 500)
         },
         init: function () {
 
@@ -59,7 +59,7 @@ const swiperHedaer = new Swiper('.header-slider-videos .swiper', {
             var h1Element = currentSlide.querySelector('h1')
             setTimeout(function () {
               $(h1Element).fadeIn()
-            }, 750)
+            }, 500)
 
             const videos = document.querySelectorAll('.header-slider-videos video')
             const activeIndex = this.realIndex
